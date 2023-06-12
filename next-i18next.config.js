c741e2028508d5863c7309431f6480b24135ed95
Areg <areghakobyan03@gmail.com>
2023-06-12 16:49:37 +0400

    test10

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,10 @@
+const path = require("path");
+
 module.exports = {
     i18n: {
         defaultLocale: 'am',
         locales: ['am', 'en', 'ru'],
         localeDetection: false,
     },
-
+    localePath: path.resolve("./public/locales"),
 };
